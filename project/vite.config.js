aeb0c8e2fbe0a53d5a8f3c4c4a9770fd7486c345
Joie-Dorine-Habarugira-dodo <joiedorine.habarugira22a@kepler.org>
2025-09-09 21:14:57 +0200

    netlify issues

--- project/vite.config.js
+++ project/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  base: '/',
   plugins: [react(), visualizer({ open: true })],
 });
