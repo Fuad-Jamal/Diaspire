de9225704706f42f7a0d2344892325365fca0ed5
Fuad Jamal <jamalfuad34@gmail.com>
2025-09-10 20:40:24 +0200

    correcting the chatwindow

--- project/src/utilities/chatWrapper.jsx
+++ project/src/utilities/chatWrapper.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import ChatWindow from "./ChatWindow";
+import ChatWindow from "./chatWindow.jsx";
 
 function ChatWrapper() {
   const { conversationId } = useParams();
